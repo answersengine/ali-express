ebafb2e36a8601d930ef8bed14059a7ffb002902
David Lynam <dlynam@gmail.com>
2019-01-23 12:39:40 -0800

    add listings parser

--- parsers/listings.rb
+++ parsers/listings.rb
@@ -5,7 +5,7 @@ products = nokogiri.css('#list-items li')
 products.each do |product|
   a_element = product.at_css('a.product')
   if a_element
-    url = URI.join('https:', a_element['href']).to_s
+    url = URI.join('https:', a_element['href']).to_s.split('?').first
     if url =~ /\Ahttps?:\/\//i
       pages << {
           url: url,
@@ -21,10 +21,12 @@ end
 #load paginated links
 pagination_links = nokogiri.css('#pagination-bottom a')
 pagination_links.each do |link|
-  url = URI.join('https:', a_element['href']).to_s
-  pages << {
-      url: url,
-      page_type: 'listings',
-      vars: {}
-    }
+  l_val = link.text.strip
+  if l_val !~ /next|previous/i && l_val.to_i < 11 #limit pagination to 10 pages
+    url = URI.join('https:', link['href']).to_s.split('?').first
+    pages << {
+        url: url,
+        page_type: 'listings'
+      }
+  end
 end
