1d3a318ec209f2208e95bce8753b1e32923b8a40
David Lynam <dlynam@gmail.com>
2019-01-28 10:05:29 -0800

    remove browser fetch_type from seeder

--- seeder/seeder.rb
+++ seeder/seeder.rb
@@ -4,7 +4,6 @@ pages << {
   headers: {"User-Agent" => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"},
   url: "https://www.aliexpress.com/category/100003109/women-clothing.html",
   force_fetch: true,
-  fetch_type: 'browser',
   vars: {
     category: "Women's clothing"
   }
